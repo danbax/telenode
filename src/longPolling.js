ee8c0e7861d0a2c2ca7db0bbe9709272e1d4b07e
Daniel Bach <danik.bachnov@gmail.com>
2024-02-13 18:27:20 +0200

    sequential update processing in long polling to improve reliability and efficiency

--- src/longPolling.js
+++ src/longPolling.js
@@ -1,48 +1,51 @@
 const axios = require('axios');
 
 const longPoll = async ({ bot, pollingDelay = 1000, cleanPreviousUpdates = true, url }) => {
-	if (pollingDelay < 50) {
-		throw new Error('Polling delay must be at least 50ms');
-	}
-	let offset = 0;
-	if (cleanPreviousUpdates) {
-		const recentUpdates = await getUpdates(url, -1);
-		offset = recentUpdates[0]?.update_id + 1 || 0;
-	}
-	while (bot.useLongPolling) {
-		try {
-			const updates = await getUpdates(url, offset);
-			updates.forEach(update => {
-				bot.telenodeHandler(update);
-				offset = update.update_id + 1;
-			});
-		} catch (err) {
-			if (err.name === 'AxiosError') {
-				console.error(
-					'Request error in long polling:\n',
-					err.message,
-					'\n',
-					err.response.data,
-				);
-			} else {
-				// If the handlers throw an error and it is not async, it will be caught here
-				console.error(err);
-			}
-		}
-		await sleep(pollingDelay);
-	}
-	console.log('Long polling stopped...');
+    if (pollingDelay < 50) {
+        throw new Error('Polling delay must be at least 50ms');
+    }
+    let offset = 0;
+    if (cleanPreviousUpdates) {
+        const recentUpdates = await getUpdates(url, -1);
+        offset = recentUpdates[0]?.update_id + 1 || 0;
+    }
+
+    let attempts = 0;
+    const maxAttempts = 5;
+
+    while (bot.useLongPolling) {
+        try {
+            const updates = await getUpdates(url, offset);
+            for (const update of updates) {
+                await bot.telenodeHandler(update);
+                offset = update.update_id + 1;
+            }
+            attempts = 0;
+        } catch (err) {
+            if (err.name === 'AxiosError') {
+                console.error('Request error in long polling:\n', err.message, '\n', err.response.data);
+                attempts++;
+                if (attempts > maxAttempts) {
+                    console.log('Max attempts reached, stopping polling...');
+                    break;
+                }
+            } else {
+                console.error(err);
+            }
+        }
+        const backoffDelay = attempts > 0 ? Math.min(1000 * (2 ** attempts), 30000) : pollingDelay;
+        await sleep(backoffDelay);
+    }
+    console.log('Long polling stopped...');
 };
 
 const getUpdates = async (url, offset) => {
-	const res = await axios.get(url + '/getUpdates', {
-		params: { offset },
-	});
-	return res.data.result;
+    const res = await axios.get(url + '/getUpdates', { params: { offset } });
+    return res.data.result;
 };
 
 module.exports = {
-	longPoll,
+    longPoll,
 };
 
-const sleep = ms => new Promise(r => setTimeout(r, ms));
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
